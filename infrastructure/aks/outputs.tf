737ff8e51cc661a03f767b5c7407e2ad99c6088c
Dhruv Parmar <35dhruvparmar@gmail.com>
2025-04-14 19:57:01 -0400

    CI/CD1

---
+++ infrastructure/aks/outputs.tf
@@ -0,0 +1,26 @@
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "kube_config" {
+  value = {
+    host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+    client_certificate     = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
+    client_key             = azurerm_kubernetes_cluster.aks.kube_config[0].client_key
+    cluster_ca_certificate = azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate
+  }
+  sensitive = true
+}
+
+
+output "aks_id" {
+  value = azurerm_kubernetes_cluster.aks.id
+}
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
