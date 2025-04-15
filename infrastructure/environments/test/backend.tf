460b4206989dea85445e8fd81eb618642ce6c910
Dhruv Parmar <35dhruvparmar@gmail.com>
2025-04-14 20:15:44 -0400

    CI/CD45

--- infrastructure/environments/test/backend.tf
+++ infrastructure/environments/test/backend.tf
@@ -1,8 +1,49 @@
 terraform {
-  backend "azurerm" {
+  required_version = "~> 1.5"
+ 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+backend "azurerm" {
     resource_group_name  = "parm0100-githubactions-rg"
     storage_account_name = "parm0100githubactions"
     container_name       = "tfstate"
     key                  = "test.terraform.tfstate"
+    use_oidc = true
+
+}
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
+
+
+
+
+terraform {
+  required_version = "~> 1.5"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.96.0"
+    }
   }
+  backend "azurerm" {
+    storage_account_name = "mckennrgithubactions"
+    container_name       = "tfstate"
+    key                  = "prod.app.tfstate"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
 }
