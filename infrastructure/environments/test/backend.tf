7ed4ed5352f31557b7abe700df8f9f72aa057eee
Dhruv Parmar <35dhruvparmar@gmail.com>
2025-04-14 20:21:05 -0400

    CI/CD7

--- infrastructure/environments/test/backend.tf
+++ infrastructure/environments/test/backend.tf
@@ -24,26 +24,3 @@ provider "azurerm" {
 }
 
 
-
-
-
-terraform {
-  required_version = "~> 1.5"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.96.0"
-    }
-  }
-  backend "azurerm" {
-    storage_account_name = "mckennrgithubactions"
-    container_name       = "tfstate"
-    key                  = "prod.app.tfstate"
-    use_oidc             = true
-  }
-}
-
-provider "azurerm" {
-  features {}
-  use_oidc = true
-}
