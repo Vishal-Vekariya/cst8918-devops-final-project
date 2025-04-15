276a5479ea70a201caa79fbda92da7188a5113e4
Dhruv Parmar <35dhruvparmar@gmail.com>
2025-04-14 20:19:16 -0400

    CI/CD6

--- infrastructure/environments/test/versions.tf
+++ infrastructure/environments/test/versions.tf
@@ -1,18 +1,18 @@
 
-provider "azurerm" {
-  features {}
-  subscription_id = "aa8bf277-fdd4-4ec4-bcd4-3458ddb8af6c" # your correct sub ID
-}
+#provider "azurerm" {
+ # features {}
+ # subscription_id = "aa8bf277-fdd4-4ec4-bcd4-3458ddb8af6c" # your correct sub ID
+#}
 
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.70.0, < 4.0.0"
-    }
-  }
-}
+#terraform {
+ # required_providers {
+  #  kubernetes = {
+   #   source  = "hashicorp/kubernetes"
+    ##  version = "~> 2.0"
+    #}
+    #azurerm = {
+     # source  = "hashicorp/azurerm"
+      #version = ">= 3.70.0, < 4.0.0"
+    #}
+  #}
+#}
