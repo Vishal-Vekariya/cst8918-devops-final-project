fc2f40253ee4df55f7f8fd01e8bc364cf36dc9ad
Dhruv Parmar <35dhruvparmar@gmail.com>
2025-04-14 20:02:58 -0400

    CI/CD2

--- infrastructure/environments/test/versions.tf
+++ infrastructure/environments/test/versions.tf
@@ -11,3 +11,16 @@ provider "azurerm" {
   features {}
   subscription_id = "aa8bf277-fdd4-4ec4-bcd4-3458ddb8af6c" # your correct sub ID
 }
+
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
+}
